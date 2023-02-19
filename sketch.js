7c84dcc4114e4c464f27d115c5705dbb1698bb17
Mark Sherman <shermanm@emmanuel.edu>
2023-02-19 16:27:22 -0500

    change to p5.party!

--- sketch.js
+++ sketch.js
@@ -1,65 +1,39 @@
-var peer;
-var id1 = '6ohdhN5857gg4c';
-var id2 = 't7n288J3QWky1i';
-var checkbox;
-var playerRadio;
-var goButton;
-var conn;
+/* globals partyConnect partyLoadShared partySetShared  partyIsHost */
+
+let checkbox;
+let shared;
+
+function preload() {
+  partyConnect(
+    "wss://deepstream-server-1.herokuapp.com",
+    "dungeonsweeper_9fRcb64hhCySEeRx",
+    "main"
+  );
+  shared = partyLoadShared('state', {checked: true});
+}
 
 function setup() {
   noCanvas();
-  playerRadio = createRadio();
-  playerRadio.option('1');
-  playerRadio.option('2');
-  playerRadio.selected('1');
-
-  goButton = createButton('Start');
-  goButton.mousePressed(start);
 
   checkbox = createCheckbox();
   checkbox.changed(checkedEvent);
+  checkbox.checked(shared.checked);
 
-}
-
-function start() {
-  console.log("Starting");
-  let player = playerRadio.value();
-  let peerId;
-  if (player === '1'){
-    peerId = id1;
-    console.log('Player 1');
-  } else {
-    peerId = id2;
-    console.log('Player 2');
+  if(partyIsHost()) {
+    console.log("Hosting")
+    partySetShared(shared, {checked: true, init: true});
   }
-  peer = new Peer(peerId);
-
-  peer.on('connection', function(conn) {
-    console.log('Connected by ', conn.peer)
-    conn.on('open', () => {
-      conn.on('data', (data) => console.log('data received:', data));
-    });
-  });
-
-  peer.on('open', function(id) {
-    console.log('My peer ID is: ' + id);
-    if (player === '1') {
-      conn = peer.connect(id2);
-    }
-  });
-
-  
 }
 
 function checkedEvent() {
-  if(this.checked()) {
-    console.log('Checking');
+  console.log(`Local change, now ${checkbox.checked()}`);
+  shared.checked = checkbox.checked();
+}
 
-  } else {
-    console.log('Unchecking');
-  }
-  if (conn._open === true) {
-    conn.send(this.checked());
+function draw() {
+  // console.log(`${millis()} ${checkbox.value()}`);
+  if(shared.checked != checkbox.checked()) {
+    checkbox.checked(shared.checked);
   }
 }
 
