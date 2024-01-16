2d6a1cf0320bcd508dda3db5abc1bff4a9659de5
Mark Sherman <shermanm@emmanuel.edu>
2024-01-16 11:40:52 -0500

    Here we go

--- sketch.js
+++ sketch.js
@@ -3,6 +3,9 @@
 let checkbox;
 let shared;
 
+let viewPort = {w: 640, h: 480}
+let gridSize = 20;
+
 function preload() {
   partyConnect(
     "wss://deepstream-server-1.herokuapp.com",
@@ -12,17 +15,12 @@ function preload() {
   shared = partyLoadShared('state', {checked: true});
 }
 
+let sqSprite;
+
 function setup() {
-  noCanvas();
+  new Canvas(viewPort.w, viewPort.h);
+  sqSprite = new Sprite(0,0,gridSize, gridSize);
 
-  checkbox = createCheckbox();
-  checkbox.changed(checkedEvent);
-  checkbox.checked(shared.checked);
-
-  if(partyIsHost()) {
-    console.log("Hosting")
-    partySetShared(shared, {checked: true, init: true});
-  }
 }
 
 function checkedEvent() {
@@ -30,10 +28,32 @@ function checkedEvent() {
   shared.checked = checkbox.checked();
 }
 
-function draw() {
-  // console.log(`${millis()} ${checkbox.value()}`);
-  if(shared.checked != checkbox.checked()) {
-    checkbox.checked(shared.checked);
+function drawMap() {
+  background(150);
+  camera.on();
+  for (let i = 0; i < 10; i++) {
+		fill(i * 20, 200, 200); // blue to gray
+		rect(-250 + i * 50, -250 + i * 100, 750, 50);
+	}
+  drawMapGrid();
+
+	if (mouse.pressing()) camera.zoom = 1.5;
+	else camera.zoom = 1;
+
+  camera.off();
+  // print(`cam x: ${camera.pos.x} y: ${camera.pos.y} mouse x: ${camera.mouse.x}/${mouseX} y: ${camera.mouse.y}/${mouseY}`);
+}
+
+function drawMapGrid() {
+  for(let x = 0; x <= width; x += gridSize) {
+    line(x, 0, x, height);
   }
+  for(let y = 0; y <= height; y += gridSize) {
+    line(0, y, width, y);
+  }
+}
+
+function draw() {
+  drawMap();
 }
 
